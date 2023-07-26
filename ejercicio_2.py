039c308c39c3dbc2a1e54f48e0161e03c9a6f8c8
Pans02 <140652135+Pans02@users.noreply.github.com>
2023-07-26 13:24:02 -0600

    ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,7 +1,18 @@
 #Roberto Carrasco Abarca 20.707.445-4
-def area (r):      
-    area=r*r*3.14;
+def areaC(r):
+  resultado = r * r * 3.14
+  return resultado
 
-radio=int(input("Ingrese el radio del circulo: "))
-Area=area(radio)
-print("El Area es:",Area)
+
+def VolC(h, r):
+  vol = h * areaC(r)
+  return vol
+
+
+radio = float(input("Ingrese el radio del circulo: "))
+Area = areaC(radio)
+
+print("El Area es:", Area)
+altura = float(input("Ingrese la altura del cilindro: "))
+volumen = VolC(altura, radio)
+print("El volumen del cilindro es:", volumen)
